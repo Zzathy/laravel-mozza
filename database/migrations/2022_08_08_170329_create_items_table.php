ff8d9a817cace2237a3d518916dd4227ade8611c
IzzaFathony <izzafathony27@gmail.com>
2022-08-09 00:13:04 +0700

    Make database

---
+++ database/migrations/2022_08_08_170329_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string("code");
+            $table->string("name");
+            $table->integer("type");
+            $table->integer("unit");
+            $table->integer("base_price");
+            $table->integer("sell_price");
+            $table->integer("stock");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+};
