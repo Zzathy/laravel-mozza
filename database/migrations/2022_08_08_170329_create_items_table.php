c61a28d07a978d6a657f7e39fb980f066b6e437c
IzzaFathony <izzafathony27@gmail.com>
2022-08-13 11:20:42 +0700

    Complete CRUD Item

--- database/migrations/2022_08_08_170329_create_items_table.php
+++ database/migrations/2022_08_08_170329_create_items_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
-            $table->string("code");
             $table->string("name");
             $table->integer("type");
             $table->integer("unit");
--- resources/views/admin/item.blade.php
+++ resources/views/admin/item.blade.php
@@ -51,26 +51,131 @@
                             </tr>
                         </tfoot>
                         <tbody>
-                            <tr>
-                                <td>Airpump AP 226</td>
-                                <td>Airator</td>
-                                <td>PCS</td>
-                                <td>21,500</td>
-                                <td>35,000</td>
-                                <td>4</td>
-                                <td>
-                                    <!-- Button trigger modal -->
-                                    <button type="button" class="btn btn-warning btn-circle btn-sm" data-toggle="modal"
-                                        data-target="#updateItem">
-                                        <i class="fas fa-pencil-alt"></i>
-                                    </button>
-                                    |
-                                    <button type="button" class="btn btn-danger btn-circle btn-sm" data-toggle="modal"
-                                        data-target="#deleteItem">
-                                        <i class="fas fa-trash"></i>
-                                    </button>
-                                </td>
-                            </tr>
+                            @foreach ($items as $item)
+                                <tr>
+                                    <td>{{ $item->name }}</td>
+                                    <td>{{ $item->type_name }}</td>
+                                    <td>{{ $item->unit_name }}</td>
+                                    <td>{{ $item->base_price }}</td>
+                                    <td>{{ $item->sell_price }}</td>
+                                    <td>{{ $item->stock }}</td>
+                                    <td>
+                                        <!-- Button trigger modal -->
+                                        <button type="button" class="btn btn-warning btn-circle btn-sm" data-toggle="modal"
+                                            data-target="#updateItem{{ $item->id }}">
+                                            <i class="fas fa-pencil-alt"></i>
+                                        </button>
+                                        |
+                                        <button type="button" class="btn btn-danger btn-circle btn-sm" data-toggle="modal"
+                                            data-target="#deleteItem{{ $item->id }}">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+
+                                <!-- Item Update Modal -->
+                                <div class="modal fade" id="updateItem{{ $item->id }}" data-backdrop="static"
+                                    data-keyboard="false" tabindex="-1" aria-labelledby="updateItemLabel"
+                                    aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="updateItemLabel">Ubah Barang</h5>
+                                                <button type="button" class="close" data-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form action="{{ route('admin.item.update', $item->id) }}" method="post">
+                                                    @csrf
+                                                    <div class="form-group">
+                                                        <label for="name">Nama Barang</label>
+                                                        <input type="text" class="form-control" name="name"
+                                                            id="name" value="{{ $item->name }}">
+                                                    </div>
+                                                    <div class="form-row">
+                                                        <div class="form-group col-md-6">
+                                                            <label for="type">Jenis</label>
+                                                            <select id="type" name="type" class="form-control">
+                                                                <option>Choose...</option>
+                                                                @foreach ($types as $type)
+                                                                    <option value="{{ $type->id }}"
+                                                                        {{ $type->id == $item->type ? 'selected' : '' }}>
+                                                                        {{ $type->name }}</option>
+                                                                @endforeach
+                                                            </select>
+                                                        </div>
+                                                        <div class="form-group col-md-6">
+                                                            <label for="unit">Satuan</label>
+                                                            <select id="unit" name="unit" class="form-control">
+                                                                <option>Choose...</option>
+                                                                @foreach ($units as $unit)
+                                                                    <option value="{{ $unit->id }}"
+                                                                        {{ $unit->id == $item->unit ? 'selected' : '' }}>
+                                                                        {{ $unit->name }}</option>
+                                                                @endforeach
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-row">
+                                                        <div class="form-group col-md-4">
+                                                            <label for="base_price">Harga Pokok</label>
+                                                            <input type="number" class="form-control" name="base_price"
+                                                                id="base_price" value="{{ $item->base_price }}">
+                                                        </div>
+                                                        <div class="form-group col-md-4">
+                                                            <label for="sell_price">Harga Jual</label>
+                                                            <input type="number" class="form-control" name="sell_price"
+                                                                id="sell_price" value="{{ $item->sell_price }}">
+                                                        </div>
+                                                        <div class="form-group col-md-4">
+                                                            <label for="stock">Stok</label>
+                                                            <input type="text" class="form-control" name="stock"
+                                                                id="stock" value="{{ $item->stock }}">
+                                                        </div>
+                                                    </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-dismiss="modal">Close</button>
+                                                <button type="submit" class="btn btn-warning">Buat</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Item Delete Modal -->
+                                <div class="modal fade" id="deleteItem{{ $item->id }}" data-backdrop="static"
+                                    data-keyboard="false" tabindex="-1" aria-labelledby="deleteItemLabel"
+                                    aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="deleteItemLabel">Hapus Barang</h5>
+                                                <button type="button" class="close" data-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p>Apakah anda yakin ingin menghapus data ini?</p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-dismiss="modal">Close</button>
+                                                <form action="{{ route('admin.item.delete', $item->id) }}"
+                                                    method="post">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <button type="submit" class="btn btn-danger">Hapus</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
@@ -80,19 +185,20 @@
     </div>
     <!-- /.container-fluid -->
 
-    <!-- Modal -->
+    <!-- Item Create Modal -->
     <div class="modal fade" id="createItem" data-backdrop="static" data-keyboard="false" tabindex="-1"
         aria-labelledby="createItemLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="createItemLabel">Modal title</h5>
+                    <h5 class="modal-title" id="createItemLabel">Tambah Barang</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    <form>
+                    <form action="{{ route('admin.item.create') }}" method="post">
+                        @csrf
                         <div class="form-group">
                             <label for="name">Nama Barang</label>
                             <input type="text" class="form-control" name="name" id="name">
@@ -100,16 +206,20 @@
                         <div class="form-row">
                             <div class="form-group col-md-6">
                                 <label for="type">Jenis</label>
-                                <select id="type" class="form-control">
-                                    <option selected>Choose...</option>
-                                    <option>...</option>
+                                <select id="type" name="type" class="form-control">
+                                    <option>Choose...</option>
+                                    @foreach ($types as $type)
+                                        <option value="{{ $type->id }}">{{ $type->name }}</option>
+                                    @endforeach
                                 </select>
                             </div>
                             <div class="form-group col-md-6">
                                 <label for="unit">Satuan</label>
-                                <select id="unit" class="form-control">
-                                    <option selected>Choose...</option>
-                                    <option>...</option>
+                                <select id="unit" name="unit" class="form-control">
+                                    <option>Choose...</option>
+                                    @foreach ($units as $unit)
+                                        <option value="{{ $unit->id }}">{{ $unit->name }}</option>
+                                    @endforeach
                                 </select>
                             </div>
                         </div>
@@ -127,92 +237,11 @@
                                 <input type="text" class="form-control" name="stock" id="stock">
                             </div>
                         </div>
-                        {{-- <button type="submit" class="btn btn-primary">Sign in</button> --}}
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-success">Buat</button>
                     </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-success">Buat</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Modal -->
-    <div class="modal fade" id="updateItem" data-backdrop="static" data-keyboard="false" tabindex="-1"
-        aria-labelledby="updateItemLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="updateItemLabel">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form>
-                        <div class="form-group">
-                            <label for="name">Nama Barang</label>
-                            <input type="text" class="form-control" name="name" id="name">
-                        </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="type">Jenis</label>
-                                <select id="type" class="form-control">
-                                    <option selected>Choose...</option>
-                                    <option>...</option>
-                                </select>
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label for="unit">Satuan</label>
-                                <select id="unit" class="form-control">
-                                    <option selected>Choose...</option>
-                                    <option>...</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-4">
-                                <label for="base_price">Harga Pokok</label>
-                                <input type="number" class="form-control" name="base_price" id="base_price">
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label for="sell_price">Harga Jual</label>
-                                <input type="number" class="form-control" name="sell_price" id="sell_price">
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label for="stock">Stok</label>
-                                <input type="text" class="form-control" name="stock" id="stock">
-                            </div>
-                        </div>
-                        {{-- <button type="submit" class="btn btn-primary">Sign in</button> --}}
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-warning">Ubah</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Modal -->
-    <div class="modal fade" id="deleteItem" data-backdrop="static" data-keyboard="false" tabindex="-1"
-        aria-labelledby="deleteItemLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="deleteItemLabel">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <p>Apakah anda yakin ingin menghapus data ini?</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-danger">Hapus</button>
                 </div>
             </div>
         </div>
