6e055127a0c91795d1ae066a8a4c791f71ddc42f
IzzaFathony <izzafathony27@gmail.com>
2022-08-20 17:10:03 +0700

    Beautify test pdf.blade.php

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -6,10 +6,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>Hello World</h1>
+    <h1>Bulanan</h1>
+    <table style="width: 100%">
+        <tr>
+            <th>Tanggal</th>
+            <th>Banyak</th>
+            <th>Total</th>
+        </tr>
+        <tr>
+            <td>20/08/2022</td>
+            <td>5</td>
+            <td>Rp150.000</td>
+        </tr>
+    </table>
+
+
+    <h1>Harian</h1>
+    <table style="width: 100%">
+        <tr>
+            <th>Tanggal</th>
+            <th>Banyak</th>
+            <th>Total</th>
+        </tr>
+        <tr>
+            <td>20/08/2022</td>
+            <td>5</td>
+            <td>Rp150.000</td>
+        </tr>
+    </table>
 </body>
 
 </html>
