2a4d093a3bae0f38cd8af17e05c3e6083d9b9589
IzzaFathony <izzafathony27@gmail.com>
2022-08-24 17:20:33 +0700

    Unnecessary changes

--- database/migrations/2022_08_24_041952_create_items_table.php
+++ database/migrations/2022_08_24_041952_create_items_table.php
@@ -16,13 +16,10 @@ return new class extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->string("name");
-
             $table->unsignedBigInteger('type_id');
             $table->foreign('type_id')->references('id')->on('types');
-
             $table->unsignedBigInteger('unit_id');
             $table->foreign('unit_id')->references('id')->on('units');
-
             $table->integer("base_price");
             $table->integer("sell_price");
             $table->float("stock");
--- database/migrations/2022_08_24_042002_create_transactions_table.php
+++ database/migrations/2022_08_24_042002_create_transactions_table.php
@@ -15,11 +15,9 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->float("quantity");
-
             $table->unsignedBigInteger('item_id');
             $table->foreign('item_id')->references('id')->on('items');
-
+            $table->float("quantity");
             $table->integer("discount");
             $table->integer("total");
             $table->timestamps();
